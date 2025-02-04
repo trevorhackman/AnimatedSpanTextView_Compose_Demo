09aaa251a00add35d1d166d276f06b63636ed2c5
Trevor <trevorhackman@gmail.com>
2025-02-03 21:03:17 -0700

    Example screen built.

--- app/src/main/java/com/example/animatedspantextviewcomposedemo/MainActivity.kt
+++ app/src/main/java/com/example/animatedspantextviewcomposedemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.animatedspantextviewcomposedemo.ui.theme.AnimatedSpanTextViewComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +23,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AnimatedSpanTextViewComposeDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    AnimatedSpanText(
+                        text = "Hello World!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun FourExamples() {
+    Column {
+        Text(
+            "RGB Normal Speed",
+            Modifier.padding(top = 12.dp),
+            fontSize = 12.sp
+        )
+        AnimatedSpanText(
+            "Hello World!",
+            fontSize = 48.sp
+        )
+        Text(
+            "RGB Fast Speed",
+            Modifier.padding(top = 12.dp),
+            fontSize = 12.sp
+        )
+        AnimatedSpanText(
+            "Hello World!",
+            fontSize = 48.sp
+        )
+        Text(
+            "Magenta Red Normal Speed",
+            Modifier.padding(top = 12.dp),
+            fontSize = 12.sp
+        )
+        AnimatedSpanText(
+            "Hello World!",
+            fontSize = 48.sp
+        )
+        Text(
+            "ROYGBIV Shorter Width Fast Speed",
+            Modifier.padding(top = 12.dp),
+            fontSize = 12.sp
+        )
+        AnimatedSpanText(
+            "Hello World!",
+            fontSize = 48.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AnimatedSpanTextViewComposeDemoTheme {
-        Greeting("Android")
+        FourExamples()
     }
 }
